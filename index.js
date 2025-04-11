e213ac1946312c525b5439ad4927e0c002045bb8
thomasKobbaConteh <thomaskobbaconteh3310@gmail.com>
2025-04-11 02:47:57 +0000

    this is another way to acess the random fn

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ function setColor(name) {
     body.style.backgroundColor = name;
 }
 
-function randomColor() {
-    const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'cyan', 'magenta', 'lime', 'teal', 'navy', 'maroon', 'olive', 'silver', 'gold', 'coral', 'salmon', 'khaki', 'plum', 'orchid', 'lavender', 'wheat', 'tan', 'peachpuff', 'lightblue', 'lightgreen', 'lightpink', 'lightsalmon', 'lightyellow', 'lightgray', 'lightcyan', 'lightcoral', 'lightseagreen', 'lightsteelblue', 'lightgoldenrodyellow', 'lightslategray', 'lightpink', 'lightblue', 'lightgreen', 'lightyellow', 'lightgray', 'lightcyan', 'lightcoral', 'lightseagreen', 'lightsteelblue', 'lightgoldenrodyellow', 'lightslategray', 'lightpink', 'lightblue', 'lightgreen', 'lightyellow', 'lightgray', 'lightcyan', 'lightcoral', 'lightseagreen', 'lightsteelblue', 'lightgoldenrodyellow', 'lightslategray'];
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    return body.style.backgroundColor = colors[randomIndex];
+// function randomColor() {
+//     const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'cyan', 'magenta', 'lime', 'teal', 'navy', 'maroon', 'olive', 'silver', 'gold', 'coral', 'salmon', 'khaki', 'plum', 'orchid', 'lavender', 'wheat', 'tan', 'peachpuff', 'lightblue', 'lightgreen', 'lightpink', 'lightsalmon', 'lightyellow', 'lightgray', 'lightcyan', 'lightcoral', 'lightseagreen', 'lightsteelblue', 'lightgoldenrodyellow', 'lightslategray', 'lightpink', 'lightblue', 'lightgreen', 'lightyellow', 'lightgray', 'lightcyan', 'lightcoral', 'lightseagreen', 'lightsteelblue', 'lightgoldenrodyellow', 'lightslategray', 'lightpink', 'lightblue', 'lightgreen', 'lightyellow', 'lightgray', 'lightcyan', 'lightcoral', 'lightseagreen', 'lightsteelblue', 'lightgoldenrodyellow', 'lightslategray'];
+//     const randomIndex = Math.floor(Math.random() * colors.length);
+//     return body.style.backgroundColor = colors[randomIndex];
     
+// }
+
+//a function to set the color of the body
+
+function randomCol() {
+    const red = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
+
+    const color = `rgb(${red}, ${green}, ${blue})`;
+    body.style.backgroundColor = color;
 }
+ randomCol();
